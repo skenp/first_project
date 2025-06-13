17e876d5fba25800ebf5929c9f6b5f1804d3efae
skenp <00mongamonga00@gmail.com>
2025-06-14 01:07:12 +0900

    마우스 제어 기능 추가
    길이 보내는 알고리즘 수정, 화면 길이 보내는것도 수정 -> struct pack 사용

--- client_socket.py
+++ client_socket.py
@@ -1,36 +1,64 @@
 import socket
 import pyautogui
+import threading
 import io
+import struct
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pyautogui.FAILSAFE = False
+
 print("write server adress")
 server_address = input()
 
-server_address_port = (server_address,8843)
+screen_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+km_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_socket.connect(server_address_port)
+screen_client_socket.connect((server_address,8843))
+
+km_client_socket.connect((server_address,8844))
 print("connected")
 
 width, height = pyautogui.size() # 화면 픽셀
 
-send_width = width.to_bytes(4, byteorder="big")
-send_height = height.to_bytes(4, byteorder="big")
+send_scale = struct.pack('>I I', width, height) # 빅엔디안 unsigned int로 바이트화
 
-send_scale = send_width + "s".encode("utf-8") + send_height # 화면 비율 인코딩 s는 구분자
+screen_client_socket.send(send_scale) # 화면 비율 보내기
 
-client_socket.send(send_scale) # 화면 비율 보내기
+def screen_send():
+    while True:
+        data=pyautogui.screenshot() # 화면 캡쳐후 image객체로 생성
 
-while True:
-    data=pyautogui.screenshot() # 화면 캡쳐후 image객체로 생성
+        byte_stream = io.BytesIO() # 이미지를 바이트로 인코딩
+        data.save(byte_stream, format='PNG')
+        send_img = byte_stream.getvalue()
 
-    byte_stream = io.BytesIO() # 이미지를 바이트로 인코딩
-    data.save(byte_stream, format='PNG')
-    send_img = byte_stream.getvalue()
+        how_bytes_long = len(send_img)
+        send_long = struct.pack('>I', how_bytes_long) # 빅엔디안 unsigned int로 바이트화
 
-    how_bytes_long_str = str(len(send_img)) # 이미지 바이트 길이 구하기
-    how_bytes_long_str = str(len(how_bytes_long_str) + int(how_bytes_long_str) + 1)+"l" # l는 구분자
+        send_data = send_long+send_img # 길이와 이미지 결합
+        screen_client_socket.send(send_data) # (바이트 길이 + 이미지) 보내기
 
-    send_long = how_bytes_long_str.encode("utf-8") # 길이를 바이트로 인코딩
+def km_receive():
+    current_mouse_state='u'
+    mouse_lmr=0
+    left_middle_right=['left','middle','right']
 
-    send_data = send_long+send_img # 길이와 이미지 결합
-    client_socket.send(send_data) # (바이트 길이 + 이미지) 보내기
+    while True:
+        km_receive_data=b''
+        while len(km_receive_data)<11: #마우스 위치,클릭 여부 받기
+            km_receive_data += km_client_socket.recv(11-len(km_receive_data))
+        mouse_x, mouse_y, mouse_lmr, mouse_down = struct.unpack('>I I H ?', km_receive_data)
+
+        pyautogui.moveTo(mouse_x, mouse_y)
+
+        if current_mouse_state=='u' and mouse_down == True:
+            current_mouse_state = 'd'
+            pyautogui.mouseDown(button=left_middle_right[mouse_lmr])
+        elif current_mouse_state=='d' and mouse_down == False:
+            current_mouse_state = 'u'
+            pyautogui.mouseUp(button=left_middle_right[mouse_lmr])
+
+screen_thread = threading.Thread(target=screen_send)
+screen_thread.start()
+
+km_thread = threading.Thread(target=km_receive)
+km_thread.start()
