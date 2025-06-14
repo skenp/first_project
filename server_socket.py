7e03223a6160705568d0022069cd26a3b92d0730
skenp <00mongamonga00@gmail.com>
2025-06-14 22:25:22 +0900

    키보드 기능 추가
    키보드 알파벳만 됨
    마우스 패킷 보내는 알고리즘 수정

--- server_socket.py
+++ server_socket.py
@@ -17,17 +17,24 @@ screen_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 screen_server_socket.bind((my_ip, 8843))
 
-mouse_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 마우스 키보드 받는 소켓 생성
+mouse_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 마우스 보내는 소켓 생성
 mouse_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 mouse_server_socket.bind((my_ip, 8844))
 
+keyboard_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 키보드 보내는 소켓 생성
+keyboard_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+keyboard_server_socket.bind((my_ip, 8845))
+
 screen_server_socket.listen() # 소켓 기다리기
 mouse_server_socket.listen()
+keyboard_server_socket.listen()
 print(my_ip+' socket waiting')
 
 screen_client_socket, client_address = screen_server_socket.accept() # 요청 받기
 mouse_client_socket, client_address = mouse_server_socket.accept()
+keyboard_client_socket, client_address = keyboard_server_socket.accept()
 print("connected from " + client_address[0]) 
 
 my_width, my_height = pyautogui.size() # 내 화면 픽셀
@@ -55,9 +62,11 @@ fps=10
 
 mouse_down = False
 mouse_lmr = False
+mouse_move = False
 
+keyboard_input = 0
 
-def screen_get(): # 화면받는 함수
+def screen_get(): # 화면받는 함수, pygame 이벤트 처리
     screen = pygame.display.set_mode((width_scale, height_scale))
     pygame.display.set_caption("screen sharing")
     pygame.init()
@@ -65,6 +74,8 @@ def screen_get(): # 화면받는 함수
 
     global mouse_down
     global mouse_lmr
+    global mouse_move
+    global keyboard_input
 
     while 1:
         clock.tick(fps)
@@ -75,6 +86,8 @@ def screen_get(): # 화면받는 함수
                 screen_client_socket.close()
                 mouse_client_socket.close()
                 sys.exit()
+            if event.type == pygame.MOUSEMOTION: #마우스 움직임 확인
+                mouse_move=True
             if event.type == pygame.MOUSEBUTTONDOWN: # 마우스 클릭 확인
                 if event.button == 1: # 왼쪽 클릭
                     mouse_lmr = 0
@@ -85,6 +98,10 @@ def screen_get(): # 화면받는 함수
                 mouse_down = True
             if event.type == pygame.MOUSEBUTTONUP:
                 mouse_down = False
+            if event.type == pygame.KEYDOWN:
+                keyboard_input = int(event.key)
+            if event.type == pygame.KEYUP:
+                keyboard_input = 0
 
         header=b'' # 길이 먼저 받기
         while len(header)<4:
@@ -106,22 +123,56 @@ def screen_get(): # 화면받는 함수
         pygame.display.update()
 
 def mouse_send():
+    global mouse_down
+    global mouse_lmr
+    global mouse_move
+
+    current_mouse_change = [mouse_down, mouse_lmr]
     while 1:
         mouse_x, mouse_y = pygame.mouse.get_pos() # 마우스 위치 확인
 
-        global mouse_down
-        global mouse_lmr
-
         mouse_x = int(mouse_x / correction) # 마우스 위치 조절
         mouse_y = int(mouse_y / correction) 
+        
 
         send_mouse_pos = struct.pack('>I I H ?', mouse_x, mouse_y, mouse_lmr, mouse_down) # 보내기
-        mouse_client_socket.send(send_mouse_pos)
 
-        time.sleep(0.25)
+        if mouse_move == True or mouse_down != current_mouse_change[0] or mouse_lmr != current_mouse_change[1]: #마우스에 변화가 있을때만 보내기
+            mouse_client_socket.send(send_mouse_pos)
+            mouse_move = False
+
+        current_mouse_change[0] = mouse_down
+        current_mouse_change[1] = mouse_lmr
+
+        time.sleep(0.05)
+
+def keyboard_send():
+    global keyboard_input
+    key_down_send = False
+
+    while(True):
+        current_key = keyboard_input
+        send_key = struct.pack('> I', current_key)
+
+        if current_key!=0 and key_down_send == False: # 키보드 입력시만 보내기 그리고 꾹 누르고 있으면 보내지 않기
+            keyboard_client_socket.send(send_key)
+            key_down_send = True
+            print('first ',end='')
+            print(current_key)
+
+        elif current_key==0 and key_down_send == True: # 키를 때면 전송
+            keyboard_client_socket.send(send_key) # 0전송 -> 키 올리라는 명령 전송
+            key_down_send = False
+            print('second ',end='')
+            print(current_key)
+        time.sleep(0.05)
+
 
 screen_thread = threading.Thread(target=screen_get)
 screen_thread.start()
 
 mouse_thread = threading.Thread(target=mouse_send)
 mouse_thread.start()
+
+keyboard_thread = threading.Thread(target=keyboard_send)
+keyboard_thread.start()
